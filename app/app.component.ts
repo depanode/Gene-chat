c438f7c64701a281783b1cde9a1220bec3e9f861
depanode <deparus@gmail.com>
2016-07-23 14:05:53 +0300

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by argho on 23.07.2016.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>hello</h1>`
+})
+
+export class AppComponent{
+
+}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by argho on 23.07.2016.
+ */
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
